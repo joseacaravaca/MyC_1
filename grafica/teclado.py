ad6a91e57913861ab63ca9db9fa02b2ca8a5d3a7
joseacaravaca <75909194+joseacaravaca@users.noreply.github.com>
2022-03-29 22:02:29 +0200

    1234

--- grafica/teclado.py
+++ grafica/teclado.py
@@ -1,67 +1,88 @@
+from ast import Return
+from io import BytesIO
+from itertools import product
 import re
 import time
 import tkinter as tk
-from tkinter import ttk
+from tkinter import NW, LabelFrame, ttk
+from tkinter import END, Label
 from xml.sax.handler import feature_validation
-
-from soupsieve import select
+import requests as req
 from MiDat import socio
-puerta="01"
+from PIL import Image, ImageTk
 
+#Ajustes locales
+puerta="01"
+url_a="http://www.ceeldense.es/cee_2/qazxsw84/foto/socee/"
+
+#Creación del formulario
 ventana=tk.Tk()
 ventana.title("CONTROL DE ACCESOS")
 ventana.geometry("600x400")
+ventana.resizable(False,False)
 
+#Cuadros de texto
 res=tk.StringVar()
 fecha = ttk.Entry()
 nombre=ttk.Entry()
 datos=ttk.Entry()
 msg=ttk.Entry()
+#Cuadro de texto capturar entrada de lector o manual
 chip=ttk.Entry(textvariable=res)
 
-
-# Posicionar
+# Posicionar cuadros de texto
 fecha.place(x=50, y=50)
 chip.pack()
 nombre.pack()
 datos.pack()
 msg.pack()
+
+#Foco a entrada de chip
 chip.focus_set()
 
-
-
-
-def resultado(event):
+def borrar():
    fecha.delete("0","end")
    nombre.delete("0","end")
    datos.delete("0","end")
    msg.delete("0","end")
-   numchip=res.get()
-   if numchip.isnumeric():   
-      p=re.compile("\d")
-      fecha.insert(0,time.strftime("%I:%M:%S"))
-      nchip="".join(p.findall(numchip))[-6:]
-      rsocio=socio(nchip,puerta)
-      if rsocio.empty:
-            nombre.insert(0,"CHIP:" + nchip + " -> no encontrado")
-      else:
-         nombre.insert(0,rsocio.at[0,"torn_nomb"] + " " +rsocio.at[0,"torn_apel"])
-         chip.delete("0","end")
-         #Estado del socio con respecto a la puerta
-         msg.insert(0,estado(rsocio.at([0,"torn_pu"+ puerta])))
-         
-
-
-         chip.focus_set
-
-
-chip.bind("<Return>", resultado)
-
-ventana.mainloop()
 
 def estado(caso):
-   estado={
-        "00":"no esta dado de alta",
-        "11":"correcto",
-        "10":"problema cuota",
-         "12":"acceso denegado"}
+   if caso=="00":
+     ret="no esta dado de alta"
+   elif caso=="11":
+      ret="correcto"
+   elif caso=="10":
+      ret="cuota impagada"
+   elif caso=="12":
+      ret="bloqueado"
+   else:
+      ret="desconocido"
+   return ret
+
+#Funcion que se ejecuta al validar la entrada de chip
+def resultado(event):
+   fecha.insert(0,time.strftime("%I:%M:%S"))
+   numchip=res.get()
+
+   if numchip.isnumeric():   
+      p=re.compile("\d")
+      nchip="".join(p.findall(numchip))[-6:]
+      rsocio=socio(nchip,puerta)
+
+      if rsocio.empty:
+            nombre.insert(0,"no encontrado")
+           
+      else:
+         nombre.insert(0,rsocio.at[0,"torn_nomb"] + " " +rsocio.at[0,"torn_apel"])
+         #Estado del socio con respecto a la puerta
+         msg.insert(0,estado(str(rsocio.at[0,"torn_pu"+puerta])))
+   ventana.after(3000,borrar)
+
+chip.focus_set
+chip.bind("<Return>", resultado)
+ventana.mainloop()
+
+
+
+
+   
