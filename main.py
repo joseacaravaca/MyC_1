0fed1a4c66964413c90910fddde1857080fc0f41
joseacaravaca <75909194+joseacaravaca@users.noreply.github.com>
2022-04-01 13:55:44 +0200

    Con control de imagen

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 #Version 1.0
+from ast import If
 import re
 import time
 import tkinter as tk
+import urllib.request
 from tkinter import NW, Label, LabelFrame, ttk
 from MiDat import socio
 from PIL import Image, ImageTk
-import urllib.request
-
 
 #Ajustes locales
 puerta="01"
 url_a="http://www.ceeldense.es/cee_2/qazxsw84/foto/socee/"
+hfoto=150 #alto de la foto
+wmaxfoto=hfoto/1.4 #Ancho a partir del que la foto se recorta
 
 #Creación del formulario
 ventana=tk.Tk()
 ventana.title("CONTROL DE ACCESOS")
 ventana.geometry("600x400")
 ventana.resizable(False,False)
+ventana.attributes('-zoomed',False)
+
+# FRAME FOTO
+frame = LabelFrame(ventana, text='FOTO SOCIO')
+frame.pack(anchor=NW)
 
 #Cuadros de texto
 res=tk.StringVar()
@@ -24,18 +31,19 @@ fecha = ttk.Entry()
 nombre=ttk.Entry()
 datos=ttk.Entry()
 msg=ttk.Entry()
+
 #Cuadro de texto capturar entrada de lector o manual
 chip=ttk.Entry(textvariable=res)
 
 # Posicionar cuadros de texto
-fecha.place(x=50, y=50)
+fecha.pack()
 chip.pack()
 nombre.pack()
 datos.pack()
 msg.pack()
 
 #Foco a entrada de chip
-chip.focus_set()
+chip.focus_set()   
 
 def borrar():
    fecha.delete("0","end")
@@ -59,15 +67,18 @@ def estado(caso):
    return ret
 
 
-# FRAME FOTO
-frame = LabelFrame(ventana, text='FOTO SOCIO')
-frame.pack(anchor=NW)
-
-
-
 # IMAGEN POR DEFECTO
 PIL_image = Image.open('sin-imagen.jpg')
-PIL_image = PIL_image.resize((100, 100), Image.ANTIALIAS)
+hpercen = (hfoto / float(PIL_image.size[1]))
+w = int((float(PIL_image.size[0]) * float(hpercen)))
+PIL_image = PIL_image.resize((w, hfoto),Image.NEAREST)
+if PIL_image.width > wmaxfoto:
+   l=(PIL_image.width-wmaxfoto)/2
+   t=0
+   r=l+wmaxfoto
+   b=hfoto
+   PIL_image=PIL_image.crop(l,t,r,b)
+
 img = ImageTk.PhotoImage(PIL_image)
 label2 = Label(frame, image=img)
 label2.image = img  # keep a reference!
@@ -78,14 +89,18 @@ def foto_socio(rfoto):
     global img
     if rfoto=="sin-imagen.jpg":
       img = Image.open('sin-imagen.jpg')
-      img = img.resize((100, 100), Image.ANTIALIAS)
+      hpercen = (hfoto / float(img.size[1]))
+      w = int((float(img.size[0]) * float(hpercen)))
+      img = img.resize((w, hfoto),Image.NEAREST)
       img = ImageTk.PhotoImage(img)
       label2.config(image=img)
     else:
         try:
             urllib.request.urlretrieve(url_a + rfoto, "tmp.jpg")
             img = Image.open('tmp.jpg')
-            img = img.resize((100, 100), Image.ANTIALIAS)
+            hpercen = (hfoto / float(img.size[1]))
+            w = int((float(img.size[0]) * float(hpercen)))
+            img = img.resize((w, hfoto),Image.NEAREST)
             img = ImageTk.PhotoImage(img)
             label2.config(image=img)
         except:
@@ -105,7 +120,6 @@ def resultado(event):
       if rsocio.empty:
             nombre.insert(0,"no encontrado")
             foto_socio("sin-imagen.jpg")
-
            
       else:
          nombre.insert(0,rsocio.at[0,"torn_nomb"] + " " +rsocio.at[0,"torn_apel"])
