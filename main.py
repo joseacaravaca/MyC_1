b3fd6850d75d4d6f7b052d1dbea6afd336688e81
joseacaravaca <75909194+joseacaravaca@users.noreply.github.com>
2022-04-05 21:41:09 +0200

    version 1.2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#Version 1.1
+#Version 1.2
 import re
-from string import ascii_letters
 import sys
 import time
 import tkinter as tk
@@ -12,9 +11,9 @@ from PIL import Image, ImageTk
 #Ajustes locales
 puerta="01"
 url_a="http://www.ceeldense.es/cee_2/qazxsw84/foto/socee/"
-hfoto=180 #alto de la foto
+hfoto=200 #alto de la foto
 wmax=hfoto/1.4 #Ancho a partir del que la foto se recorta
-lico=150 #Medidas de icono informativo
+lico=100 #Medidas de icono informativo
 
 #Creación del formulario
 ventana=tk.Tk()
@@ -22,26 +21,28 @@ ventana.title("CONTROL DE ACCESOS")
 ventana.geometry("800x400")
 ventana.resizable(True,True)
 ventana.attributes('-fullscreen',True)
+ventana.wm_attributes("-transparentcolor", 'red')
 
 #Cuadros de texto
 res=tk.StringVar()
 fecha = ttk.Label()
 nombre=ttk.Label(font=('Arial',36))
 apellido=ttk.Label(font=('Arial',36))
-datos=ttk.Label()
 msg=ttk.Label(font=('Arial',36))
+datos=ttk.Label(font=('Arial',30))
 iminf=ttk.Label()
 
 #Cuadro de texto capturar entrada de lector o manual
 chip=ttk.Entry(textvariable=res)
 
 # Posicionar cuadros de texto
-nombre.place(x=150,y=30)
-apellido.place(x=150,y=80)
+nombre.place(x=180,y=30)
+apellido.place(x=180,y=80)
+msg.place(x=180,y=130)
+datos.place(x=180,y=190)
+
 fecha.place(x=0,y=300)
 chip.place(x=10,y=360)
-datos.place(x=30,y=300)
-msg.place(x=10,y=300)
 
 #Foco a entrada de chip
 chip.focus_set()   
@@ -60,30 +61,34 @@ def icoinfo(ep):
    #Icono informativo cambia segun el estado del socio
    global imx
    if ep=="11":
-         im=ajustar_imagen(lico,lico,Image.open('verde.jpg'))
+         im=ajustar_imagen(lico,lico,Image.open('verde.png'))
          imx = ImageTk.PhotoImage(im)
          iminf.config(image=imx)
    elif ep=="borra":
-         im=ajustar_imagen(lico,lico,Image.open('nada.jpg'))
+         im=ajustar_imagen(lico,lico,Image.open('nada.png'))
          imx = ImageTk.PhotoImage(im)
          iminf.config(image=imx)
    else:
-         im=ajustar_imagen(lico,lico,Image.open('rojo.jpg'))
+         im=ajustar_imagen(lico,lico,Image.open('rojo.png'))
          imx = ImageTk.PhotoImage(im)
          iminf.config(image=imx)
      
 def estado(caso):
    if caso=="00":
-     ret="no esta dado de alta"
+     ret="Acceso no permitido"
+     ms="No está de alta en este acceso"
    elif caso=="11":
-      ret="correcto "
+      ret="Adelante, pase... "
+      ms=""
    elif caso=="10":
-      ret="cuota impagada"
+      ret="Acceso no permitido"
+      ms="Cuota incorrecta, consulte"
    elif caso=="12":
-      ret="bloqueado"
+      ret="Acceso no permitido"
+      ms="Acceso bloquedo, consulte"
    else:
       ret="desconocido"
-   return ret
+   return [ret,ms]
 
 def ajustar_imagen(ancho,alto,imagen):
    hpercen = (alto/ float(imagen.size[1]))
@@ -103,10 +108,10 @@ img = ImageTk.PhotoImage(PIL_image)
 fotosocio = Label(ventana, image=img)
 fotosocio.place(x=10,y=10)
 
-im=ajustar_imagen(lico,lico,Image.open('nada.jpg'))
+im=ajustar_imagen(lico,lico,Image.open('nada.png'))
 imx = ImageTk.PhotoImage(im)
 iminf=Label(ventana, image=imx)
-iminf.place(x=250,y=210)
+iminf.place(x=350,y=250)
 
 #FOTO DE SOCIO
 def foto_socio(rfoto):
@@ -138,7 +143,8 @@ def resultado(event):
       rsocio=socio(nchip,puerta)
 
       if rsocio.empty:
-            nombre.configure(text="no encontrado")
+            msg.configure(text="Acceso no permitido")
+            datos.configure(text="Tarjeta no válida")
             foto_socio("sin-imagen.jpg")
             icoinfo("rojo")        
       else:
@@ -146,7 +152,8 @@ def resultado(event):
          nombre.configure(text=rsocio.at[0,"torn_nomb"])
          apellido.configure(text=rsocio.at[0,"torn_apel"])
          #Estado del socio con respecto a la puerta
-         msg.configure(text=estado(rpuerta))
+         msg.configure(text=estado(rpuerta)[0])
+         datos.configure(text=estado(rpuerta)[1])
          foto_socio(rsocio.at[0,"torn_foto"])
          icoinfo(rpuerta)
    else:
